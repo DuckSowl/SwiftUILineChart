9c3b9b521e4e7ffa8c0215bc6badeab352ae10fe
Anton Tolstov <anton@macbook.local>
2020-03-18 17:40:09 +0300

    Add iOS 13 compatibility

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUILineChart",
+    platforms: [
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "SwiftUILineChart",
